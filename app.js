c9575ffc91310bd36610dff590aa9396ccccf534
sulo-git12 <sulo-git12@DESKTOP-O7FKKKV>
2022-09-30 11:09:34 +0530

    Created Order Model and Route

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ const PORT = process.env.APP_RUNNING_PORT || 3000;
 //Add Routes
 const outletRouter = require("./src/routes/outlet");
 const food_itemRouter = require("./src/routes/food_Item");
+const orderRouter = require("./src/routes/order");
 
 // Add Middlewares
 app.use(express.json());
+<<<<<<< HEAD
 <<<<<<< HEAD
 app.use("/outlets", outletRouter);
 app.use("/foods", food_itemRouter);
@@ -34,6 +36,11 @@ mongoose
   .then(() => console.log("Successfully connected to mongodb !"))
   .catch((err) => console.log(`Error has occured: ${err}`));
 >>>>>>> b172ca198e628886ba74b4deb7ec51707bdd40ba
+=======
+app.use("/api/outlets", outletRouter);
+app.use("/foods", food_itemRouter);
+app.use("/api/orders", orderRouter);
+>>>>>>> 0d22a69 (Created Order Model and Route)
 
 // Check runing port
 app.listen(PORT, () => {
