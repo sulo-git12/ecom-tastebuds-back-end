2eb92ada83fbac51aa7ee313965406454e300de9
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-03 00:15:48 +0530

    Remove fvoulet route in app file

--- app.js
+++ app.js
@@ -14,39 +14,11 @@ const PORT = process.env.APP_RUNNING_PORT || 3000;
 
 //Add Routes
 const outletRouter = require("./src/routes/outlet");
-const food_itemRouter = require("./src/routes/food_Item");
-const orderRouter = require("./src/routes/order");
 
 // Add Middlewares
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-app.use("/outlets", outletRouter);
-app.use("/foods", food_itemRouter);
-=======
-
-// APP ROUTES
-// app.use("/yyy/xx", testRoutes);
-
-// Mongo DB Connections
-mongoose
-  .connect(process.env.MONGO_DB_CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Successfully connected to mongodb !"))
-  .catch((err) => console.log(`Error has occured: ${err}`));
->>>>>>> b172ca198e628886ba74b4deb7ec51707bdd40ba
-=======
 app.use("/api/outlets", outletRouter);
-app.use("/foods", food_itemRouter);
-app.use("/api/orders", orderRouter);
->>>>>>> 0d22a69 (Created Order Model and Route)
-=======
-app.use("/api/outlets", outletRouter);
->>>>>>> 7931c65 (Upated Outlet Model & Route)
 
 // Check runing port
 app.listen(PORT, () => {
