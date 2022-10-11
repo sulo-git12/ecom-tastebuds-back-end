fb1dc077beec825c03a082fea0df5617d1d21301
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2022-10-11 08:11:12 +0530

    Change Api end points names

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 app.use("/api/outlets", foodOutletRouter);
-app.use("/api/food_items", foodItemRouter);
+app.use("/api/foods", foodItemRouter);
 app.use("/api/favorites", favFoodOutletRouter);
-app.use("/api/my_orders", MyOrderRouter);
-app.use("/api/my_order_items", MyOrderItemRouter);
+app.use("/api/orders", MyOrderRouter);
+app.use("/api/orders/items", MyOrderItemRouter);
 
 // Check runing port
 app.listen(PORT, () => {
